a7f5927f54cac3ed7817437ff314985ee4588e79
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-09-29 12:55:38 +0800

    Rename lib.[ch] to testhelper.[ch]

--- lib/testhelper.c
+++ lib/testhelper.c
@@ -1,4 +1,4 @@
-#include "lib.h"
+#include "testhelper.h"
 
 #include <stdio.h>
 #include <string.h>
--- src/str_reverse.c
+++ src/str_reverse.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "lib.h"
+#include "testhelper.h"
 
 void reverse_str(char *str)
 {
