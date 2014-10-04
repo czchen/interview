ad9e7f34e5303a69e31e2ed73d58701646f93eeb
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-10-04 23:20:59 +0800

    Add factorial_trailing_zero.c

---
+++ src/factorial_trailing_zero.c
@@ -0,0 +1,31 @@
+/*
+ * Write an algorithm which computes the number of trailing zeros in n
+ * factorial.
+ */
+
+#include <stdio.h>
+
+#include "testhelper.h"
+
+int trailing_factorial_zero_count(int x)
+{
+    int count = 0;
+
+    while (x) {
+        x /= 5;
+        count += x;
+    }
+
+    return count;
+}
+
+int main()
+{
+    struct Test test = { 0 };
+
+    ok_int_eq(&test, 1, trailing_factorial_zero_count(5));
+    ok_int_eq(&test, 2, trailing_factorial_zero_count(10));
+    ok_int_eq(&test, 6, trailing_factorial_zero_count(25));
+
+    return get_status(&test);
+}
